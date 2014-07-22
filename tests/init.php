b2df17601ca20b231e1f2974580197eb9142c90c
Rick den Haan <r.denhaan@solware.nl>
2014-07-22 17:42:39 +0200

    Added unit test for Screen object

---
+++ tests/init.php
@@ -0,0 +1,23 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+/**
+ * Prepares a simple autoloader for the Capirussa\Pushover namespace
+ */
+
+date_default_timezone_set('Europe/Amsterdam');
+
+// handle autoloading
+spl_autoload_register(
+    function ($className) {
+        if ($className === 'MockClient') {
+            require_once(dirname(__FILE__) . '/Capirussa/Pathe/mock/MockClient.php');
+        } else if (preg_match('/^Capirussa\\\\Pathe/', $className)) {
+            $filePath = str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
+            if (file_exists($filePath)) {
+                require_once(dirname(__FILE__) . '/../' . $filePath);
+            }
+        }
+    }
+);
