4a08b46dac67049b7a8c8ed1f2995ade8c0e2dbb
Rick den Haan <r.denhaan@solware.nl>
2014-07-23 22:57:07 +0200

    Added mock client to unittest autoloader

--- tests/init.php
+++ tests/init.php
@@ -13,6 +13,8 @@ spl_autoload_register(
     function ($className) {
         if ($className === 'MockRequest') {
             require_once(dirname(__FILE__) . '/Capirussa/Pathe/mock/MockRequest.php');
+        } else if ($className === 'MockClient') {
+            require_once(dirname(__FILE__) . '/Capirussa/Pathe/mock/MockClient.php');
         } else if (preg_match('/^Capirussa\\\\Pathe/', $className)) {
             $filePath = str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
             if (file_exists($filePath)) {
